ddf8eedfa6af8d275694e49b095907f462523368
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-07-16 11:51:04 -0600

    (JS) added script template for displaying catalogue information below catalogue btns

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// QUERY SELECTORS
+const projectBtn = document.querySelector("#projects-button");
+const bootcampBtn = document.querySelector("#bootcamp-button");
+const inDevelopmentBtn = document.querySelector("#in-development-button");
+
+// FUNCTIONS
+
+function displayProject() {
+  // HTML literals for displaying Projects details
+}
+
+function displayBootcamp() {
+  // HTML literals for displaying Bootcamp Assignments details
+}
+function displayInProgress() {
+  // HTML literals for displaying In Progress details
+}
+
+// EVENT LISTENERS
+projectBtn.addEventListener("click", displayProject);
+bootcampBtn.addEventListener("click", displayBootcamp);
+inDevelopmentBtn.addEventListener("click", displayInProgress);
